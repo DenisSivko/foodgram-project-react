97caa8d3a66c1c75613f432a46f17722088fa8a9
DenisSivko <densivko2306@yandex.ru>
2021-11-05 01:06:26 +0300

    Uploading project

--- README.md
+++ README.md
@@ -1,1 +1,80 @@
-# praktikum_new_diplom
+#  Foodgram - продуктовый помощник
+
+## Описание проекта
+На этом сервисе пользователи смогут публиковать рецепты, подписываться на публикации других пользователей, добавлять понравившиеся рецепты в список «Избранное», а перед походом в магазин скачивать сводный список продуктов, необходимых для приготовления одного или нескольких выбранных блюд.
+
+## Подготовка и запуск проекта
+##### Клонирование репозитория
+Склонируйте репозиторий на локальную машину:
+```bash
+git clone https://github.com/DenisSivko/foodgram-project-react.git
+```
+
+## Установка на удаленном сервере (Ubuntu):
+##### Шаг 1. Выполните вход на свой удаленный сервер
+Прежде, чем приступать к работе, необходимо выполнить вход на свой удаленный сервер:
+```bash
+ssh <USERNAME>@<IP_ADDRESS>
+```
+
+##### Шаг 2. Установите docker на сервер:
+Введите команду:
+```bash
+sudo apt install docker.io 
+```
+
+##### Шаг 3. Установите docker-compose на сервер:
+Введите команды:
+```bash
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+```
+
+##### Шаг 4. Скопируйте подготовленные файлы:
+Скопируйте подготовленные файлы `docker-compose.yaml` и `nginx/default.conf` из вашего проекта на сервер в `home/<ваш_username>/docker-compose.yaml` и `home/<ваш_username>/nginx/default.conf` соответственно.
+Введите команду из корневой папки проекта:
+```bash
+scp docker-compose.yml <username>@<host>:/home/<username>/docker-compose.yml
+scp -r nginx/ <username>@<host>:/home/<username>/
+```
+
+##### Шаг 5. Добавьте Secrets:
+Для работы с Workflow добавьте в Secrets GitHub переменные окружения для работы:
+```bash
+DB_ENGINE=django.db.backends.postgresql
+DB_NAME=postgres
+POSTGRES_USER=postgres
+POSTGRES_PASSWORD=postgres
+DB_HOST=db
+DB_PORT=5432
+
+DOCKER_PASSWORD=<пароль DockerHub>
+DOCKER_USERNAME=<имя пользователя DockerHub>
+
+USER=<username для подключения к серверу>
+HOST=<IP сервера>
+PASSPHRASE=<пароль для сервера, если он установлен>
+SSH_KEY=<ваш SSH ключ (для получения команда: cat ~/.ssh/id_rsa)>
+
+TELEGRAM_TO=<ID своего телеграм-аккаунта>
+TELEGRAM_TOKEN=<токен вашего бота>
+```
+
+##### Шаг 6. После успешного деплоя:
+Зайдите на боевой сервер и выполните команды (только после первого деплоя):
+###### Создаем и применяем миграции:
+```bash
+sudo docker-compose exec backend python manage.py migrate --noinput
+```
+###### Подгружаем статику
+```bash
+sudo docker-compose exec backend python manage.py collectstatic --no-input 
+```
+###### Заполнить базу данных:
+```bash
+sudo docker-compose exec backend python manage.py loaddata fixtures/ingredients.json
+```
+###### Создать суперпользователя Django:
+```bash
+sudo docker-compose exec backend python manage.py createsuperuser
+```
---
+++ backend/api/serializers.py
@@ -0,0 +1,191 @@
+from drf_extra_fields.fields import Base64ImageField
+from rest_framework import serializers
+
+from api.models import (Favorite, Ingredient, IngredientQuantity, Recipe,
+                        ShoppingCart, Tag)
+from users.serializers import CustomUserSerializer
+
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'color', 'slug')
+
+
+class IngredientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+        fields = ('id', 'name', 'measurement_unit')
+
+
+class IngredientQuantitySerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='ingredient.id')
+    name = serializers.ReadOnlyField(source='ingredient.name')
+    measurement_unit = serializers.ReadOnlyField(
+        source='ingredient.measurement_unit'
+    )
+
+    class Meta:
+        model = IngredientQuantity
+        fields = ('id', 'name', 'measurement_unit', 'amount')
+
+
+class RecipeListSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, read_only=True)
+    author = CustomUserSerializer(read_only=True)
+    ingredients = serializers.SerializerMethodField(read_only=True)
+    is_favorited = serializers.SerializerMethodField(read_only=True)
+    is_in_shopping_cart = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'tags', 'author', 'ingredients', 'is_favorited',
+            'is_in_shopping_cart', 'name', 'image', 'text', 'cooking_time'
+        )
+
+    def get_ingredients(self, obj):
+        ingredients = IngredientQuantity.objects.filter(recipe=obj)
+        return IngredientQuantitySerializer(ingredients, many=True).data
+
+    def get_is_favorited(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Favorite.objects.filter(user=user, recipe=obj).exists()
+
+    def get_is_in_shopping_cart(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return ShoppingCart.objects.filter(user=user, recipe=obj).exists()
+
+
+class IngredientWriteSerializer(serializers.ModelSerializer):
+    id = serializers.PrimaryKeyRelatedField(
+        queryset=Ingredient.objects.all()
+    )
+    amount = serializers.IntegerField()
+
+    class Meta:
+        model = IngredientQuantity
+        fields = ('id', 'amount')
+
+
+class RecipeWriteSerializer(serializers.ModelSerializer):
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(), many=True
+    )
+    ingredients = IngredientWriteSerializer(many=True)
+    author = CustomUserSerializer(read_only=True)
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = (
+            'id', 'author', 'ingredients', 'tags', 'image',
+            'name', 'text', 'cooking_time'
+        )
+
+    def create_ingredients(self, ingredients, recipe):
+        if not ingredients:
+            raise serializers.ValidationError({
+                'ingredients': 'Нужно выбрать хотя бы один ингредиент!'
+            })
+
+        for ingredient in ingredients:
+            ingredient_id = ingredient['id']
+            amount = ingredient['amount']
+            if amount <= 0:
+                raise serializers.ValidationError({
+                    'amount': 'Количество ингредиента должно быть больше нуля!'
+                })
+            IngredientQuantity.objects.create(
+                recipe=recipe, ingredient=ingredient_id, amount=amount
+            )
+
+    def create_tags(self, tags, recipe):
+        for tag in tags:
+            recipe.tags.add(tag)
+
+    def create(self, validated_data):
+        author = self.context.get('request').user
+        tags = validated_data.pop('tags')
+        ingredients = validated_data.pop('ingredients')
+        recipe = Recipe.objects.create(author=author, **validated_data)
+        self.create_tags(tags, recipe)
+        self.create_ingredients(ingredients, recipe)
+        return recipe
+
+    def update(self, instance, validated_data):
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time
+        )
+
+        instance.tags.clear()
+        tags = validated_data.get('tags')
+        self.create_tags(tags, instance)
+
+        IngredientQuantity.objects.filter(recipe=instance).all().delete()
+        ingredients = validated_data.get('ingredients')
+        self.create_ingredients(ingredients, instance)
+
+        instance.save()
+        return instance
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeListSerializer(
+            instance, context=context).data
+
+
+class RecipeRepresentationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipe
+        fields = ('id', 'name', 'image', 'cooking_time')
+
+
+class FavoriteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Favorite
+        fields = ('user', 'recipe')
+
+    def validate(self, data):
+        user = self.context['request'].user
+        recipe = data['recipe']
+        if Favorite.objects.filter(user=user, recipe=recipe).exists():
+            raise serializers.ValidationError({
+                'status': 'Рецепт уже есть в избранном!'
+            })
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeRepresentationSerializer(
+            instance.recipe, context=context).data
+
+
+class ShoppingCartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShoppingCart
+        fields = ('user', 'recipe')
+
+    def validate(self, data):
+        user = self.context['request'].user
+        recipe = data['recipe']
+        if ShoppingCart.objects.filter(user=user, recipe=recipe).exists():
+            raise serializers.ValidationError({
+                'status': 'Рецепт уже есть в списке покупок!'
+            })
+        return data
+
+    def to_representation(self, instance):
+        request = self.context.get('request')
+        context = {'request': request}
+        return RecipeRepresentationSerializer(
+            instance.recipe, context=context).data
